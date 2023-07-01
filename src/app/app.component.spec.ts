38222278b830da36b9ac7a383a3dc51ee45231d5
nimashdammalage <nimash@skyledge.com>
2023-07-01 14:59:34 +1000

    a3: test async data received from a servie method

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -25,11 +25,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app.title).toEqual('ng-testing-and-debugging');
   });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('ng-testing-and-debugging app is running!');
-  });
 });
--- src/app/user-list/user-list.service.spec.ts
+++ src/app/user-list/user-list.service.spec.ts
@@ -1,16 +1,24 @@
-import { TestBed } from '@angular/core/testing';
+import {TestBed} from '@angular/core/testing';
 
-import { UserListService } from './user-list.service';
+import {UserListService} from './user-list.service';
 
+const memberCount = 16;
 describe('UserListService', () => {
   let service: UserListService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(UserListService);
+    service = new UserListService();
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  // wait until async data is received and test
+  it(`should return a user list with ${memberCount} members`, (done: DoneFn) => {
+    service.getAll().then(response => {
+      expect(response.length).toBe(memberCount);
+      done();
+    });
   });
 });
