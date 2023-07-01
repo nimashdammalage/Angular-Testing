d717474286c995186b02422f258bcd8e6c11167a
nimashdammalage <nimash@skyledge.com>
2023-07-01 14:47:41 +1000

    a2: add error handler and local storage

--- src/app/services/web-storage.service.ts
+++ src/app/services/web-storage.service.ts
@@ -1,9 +1,18 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WebStorageService {
 
-  constructor() { }
+  constructor() {
+  }
+
+  public get(key: string): string | null {
+    return window.localStorage.getItem(key);
+  }
+
+  public set(key: string, value: string): void {
+    return window.localStorage.setItem(key, value);
+  }
 }
