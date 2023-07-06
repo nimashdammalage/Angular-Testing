4d0aae6342776db17d5ca6a3ed33097b5993b8cb
nimashdammalage <nimash@skyledge.com>
2023-07-06 11:07:15 +1000

    a6: route unit testing

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,28 +1,51 @@
-import { TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
-import { AppComponent } from './app.component';
+import {fakeAsync, TestBed, tick} from '@angular/core/testing';
+import {RouterTestingModule} from '@angular/router/testing';
+import {AppComponent} from './app.component';
+import {Router, Routes} from '@angular/router';
+import {UserListComponent} from './user-list/user-list.component';
+import {ViewUserComponent} from './view-user/view-user.component';
+import {Location} from '@angular/common';
+
+const fakeRoutes: Routes = [
+  {path: 'dashboard', component: UserListComponent},
+  {path: 'user', component: ViewUserComponent},
+  {path: '', redirectTo: 'dashboard', pathMatch: 'full'},
+];
 
 describe('AppComponent', () => {
+  let router: Router;
+  let location: Location;
+  let fixture;
+  let component: AppComponent;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
+      imports: [RouterTestingModule.withRoutes(fakeRoutes)],
       declarations: [
         AppComponent
       ],
     }).compileComponents();
+
+    router = TestBed.inject(Router);
+    location = TestBed.inject(Location);
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+    router.initialNavigation();
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+    expect(component).toBeTruthy();
   });
 
-  it(`should have as title 'ng-testing-and-debugging'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('ng-testing-and-debugging');
-  });
+  it('navigate to "" redirects you to /dashboard', fakeAsync(() => {
+    router.navigate(['']);
+    tick();
+    expect(location.path()).toBe('/dashboard');
+  }));
+
+  it('navigate to "search" takes you to /user', fakeAsync(() => {
+    router.navigate(['user']);
+    tick();
+    expect(location.path()).toBe('/user');
+  }));
 });
